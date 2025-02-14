cc913b310c3cf0f5e59b96b9139e65f3bc3fa9ad
Pham Tie Hung <hungptse183180@fpt.edu.vn>
2025-02-14 22:40:12 +0700

    AddBooking...

---
+++ SWP391_BackEnd/Controllers/BookingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SWP391_BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("This is BookingController");
+        }
+    }
+}
