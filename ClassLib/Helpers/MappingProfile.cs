aec99fe147d37235ee9f54d95f8620c16d29eeaf
Alexdev257 <bthang2507@gmail.com>
2025-02-16 04:09:35 +0700

    Update register, login endpoint JWT Authorize system

---
+++ ClassLib/Helpers/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using ClassLib.DTO.User;
+using ClassLib.Models;
+
+namespace ClassLib.Helpers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<RegisterRequest, User>();
+            CreateMap<User, RegisterResponse>();
+
+            CreateMap<LoginRequest, User>();
+            CreateMap<User, LoginResponse>();
+
+        }
+    }
+}
--- ClassLib/Models/DbSwpVaccineTracking2Context.cs
+++ ClassLib/Models/DbSwpVaccineTracking2Context.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
 namespace ClassLib.Models;
-
 public partial class DbSwpVaccineTracking2Context : DbContext
 {
     public DbSwpVaccineTracking2Context()
@@ -37,7 +36,7 @@ public partial class DbSwpVaccineTracking2Context : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-8UGAAJKM\\SQLEXPRESS01;Database=DB_SWP_Vaccine_Tracking2;User Id=sa;Password=12345;TrustServerCertificate=True;" /*"data source=TieHung\\SQLEXPRESS;initial catalog=DB_SWP_Vaccine_Tracking2;user id=sa;password=123456;TrustServerCertificate=True"*/ );
+        => optionsBuilder.UseSqlServer("Server=LAPTOP-8UGAAJKM\\SQLEXPRESS01;Database=DB_SWP_Vaccine_Tracking2;User Id=sa;Password=12345;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
