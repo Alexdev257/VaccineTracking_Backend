c5229e998a6659199b4b2ad51b5a4116e370ac9e
Pham Tie Hung <hungptse183180@fpt.edu.vn>
2025-02-15 23:00:22 +0700

    test

--- ClassLib/DTO/Response/AddBooking.cs
+++ ClassLib/DTO/Response/AddBooking.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLib.Models;
 
 namespace ClassLib.DTO.Response
 {
     internal class AddBooking
     {
+        public int ParentId { get; set; }
+        public required DateTime BookingDate { get; set; }
+
+        public int TotalPrice { get; set; }
+
+        public ICollection<Child>? Children { get; set; }
+
+        public ICollection<Vaccine>? Vaccines { get; set; }
+
+        public ICollection<VaccinesCombo>?  VaccinesCombos { get; set; }
     }
 }
