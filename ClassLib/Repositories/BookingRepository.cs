b8970b4a94fabf0cd2c50498a9c3a7abe0417b76
Pham Tie Hung <hungptse183180@fpt.edu.vn>
2025-02-16 18:41:06 +0700

    Booking Method Get + Querry

--- ClassLib/Repositories/BookingRepository.cs
+++ ClassLib/Repositories/BookingRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClassLib.Helpers;
 using ClassLib.Models;
 
 using Microsoft.EntityFrameworkCore;
@@ -16,19 +17,59 @@ namespace ClassLib.Repositories
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
-        public async Task<List<Booking>?> getAll()
+        public async Task<List<Booking>?> GetByQuerry(BookingQuerryObject bookingQuerryObject)
         {
-            return await _context.Bookings.ToListAsync();
+            var booking = _context.Bookings
+                          .Include(x => x.Parent)
+                          .AsQueryable();
+
+            if (bookingQuerryObject.Id.HasValue)
+            {
+                booking = booking.Where(x => x.Id == bookingQuerryObject.Id);
+            }
+            if (bookingQuerryObject.ParentId.HasValue)
+            {
+                booking = booking.Where(x => x.ParentId == bookingQuerryObject.ParentId);
+            }
+            if (bookingQuerryObject.Status != null)
+            {
+                booking = booking.Where(x => x.Status == bookingQuerryObject.Status);
+            }
+            if (bookingQuerryObject.CreateDate.HasValue)
+            {
+                booking = booking.Where(x => x.CreatedAt == bookingQuerryObject.CreateDate);
+            }
+            if (bookingQuerryObject.PhoneNumber != null)
+            {
+                booking = booking.Where(x => x.Parent.PhoneNumber == bookingQuerryObject.PhoneNumber);
+            }
+            if (bookingQuerryObject.orderBy != null)
+            {
+                switch (bookingQuerryObject.orderBy)
+                {
+                    case "Id":
+                        booking = bookingQuerryObject.isDescending ? booking.OrderByDescending(x => x.Id) : booking.OrderBy(x => x.Id);
+                        break;
+                    case "ParentId":
+                        booking = bookingQuerryObject.isDescending ? booking.OrderByDescending(x => x.ParentId) : booking.OrderBy(x => x.ParentId);
+                        break;
+                    case "TotalPrice":
+                        booking = bookingQuerryObject.isDescending ? booking.OrderByDescending(x => x.TotalPrice) : booking.OrderBy(x => x.TotalPrice);
+                        break;
+                    case "CreatedAt":
+                        booking = bookingQuerryObject.isDescending ? booking.OrderByDescending(x => x.CreatedAt) : booking.OrderBy(x => x.CreatedAt);
+                        break;
+                    case "ArrivedAt":
+                        booking = bookingQuerryObject.isDescending ? booking.OrderByDescending(x => x.ArrivedAt) : booking.OrderBy(x => x.ArrivedAt);
+                        break;
+                    case "Status":
+                        booking = bookingQuerryObject.isDescending ? booking.OrderByDescending(x => x.Status) : booking.OrderBy(x => x.Status);
+                        break;
+                }
+            }
+
+            return await booking.ToListAsync();
         }
 
-        public async Task<List<Booking>?> getBookingByParentId(int userId)
-        {
-            return await _context.Bookings.Where(b => b.ParentId == userId).ToListAsync();
-        }
-
-        public async Task<List<Booking>?> getBookingByStatus(string status)
-        {
-            return await _context.Bookings.Where(b => b.Status == status).ToListAsync();
-        }
     }
 }
