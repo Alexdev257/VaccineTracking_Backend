949f52ae8c6851e1d698aec7ddbee795ae49355c
Alexdev257 <bthang2507@gmail.com>
2025-02-18 13:08:26 +0700

    Update Jwt

--- ClassLib/DTO/User/LoginResponse.cs
+++ ClassLib/DTO/User/LoginResponse.cs
@@ -8,13 +8,15 @@ namespace ClassLib.DTO.User
 {
     public class LoginResponse
     {
-        public int Id { get; set; }
-        public string Name { get; set; } = null!;
-        public string PhoneNumber { get; set; } = null!;
-        public string Username { get; set; } = null!;
-        //public string Password { get; set; } = null!;
-        public string Role { get; set; } = null!;
-        public int Gender { get; set; }
-        public string Token { get; set; } = null!;
+        //public int Id { get; set; }
+        //public string Name { get; set; } = null!;
+        //public string PhoneNumber { get; set; } = null!;
+        //public string Username { get; set; } = null!;
+        ////public string Password { get; set; } = null!;
+        //public string Role { get; set; } = null!;
+        //public int Gender { get; set; }
+        //public string Token { get; set; } = null!;
+        public string AccessToken { get; set; } = null!;
+        public string RefreshToken { get; set; } = null!;
     }
 }
