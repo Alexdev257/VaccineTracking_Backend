c529205e09981b9edc6fd106b09b4c155e79b7bd
Alexdev257 <bthang2507@gmail.com>
2025-02-18 15:00:40 +0700

    Update Model from  DB

---
+++ ClassLib/Models/BookingChildId.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLib.Models;
+
+public partial class BookingChildId
+{
+    public int BookingId { get; set; }
+
+    public int ChildId { get; set; }
+
+    public virtual Booking Booking { get; set; } = null!;
+
+    public virtual Child Child { get; set; } = null!;
+}
---
+++ ClassLib/Models/BookingIdVaccineId.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLib.Models;
+
+public partial class BookingIdVaccineId
+{
+    public int BookingId { get; set; }
+
+    public int VaccineId { get; set; }
+
+    public virtual Booking Booking { get; set; } = null!;
+
+    public virtual Vaccine Vaccine { get; set; } = null!;
+}
