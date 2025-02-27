852f34f4e493a2f3f61f96e4b953978b715ae011
Pham Tie Hung <hungptse183180@fpt.edu.vn>
2025-02-27 11:10:44 +0700

    test booking child

---
+++ SWP391_BackEnd/Controllers/BookingChildController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLib.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace SWP391_BackEnd.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class BookingChildController : ControllerBase
+    {
+        private readonly DbSwpVaccineTrackingContext _context;
+
+        public BookingChildController(DbSwpVaccineTrackingContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Booking>>> Get()
+        {
+            return await _context.Bookings.Include(b => b.Children).ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Booking>> AddBooking(int bookingID, int childID){
+            if( await _context.Bookings.FindAsync(bookingID) == null || await _context.Children.FindAsync(childID) == null){
+                return BadRequest();
+            }
+            var booking = _context.Bookings.FindAsync(bookingID);
+            booking.Result.Children.Add(_context.Children.FindAsync(childID).Result);
+            await _context.SaveChangesAsync();
+            return Ok(booking);
+        }
+    }
+}
