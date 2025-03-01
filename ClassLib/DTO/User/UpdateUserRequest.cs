488f321bc349d3d66e24111e0b69139e5f4b9b1a
Alexdev257 <bthang2507@gmail.com>
2025-03-01 20:54:25 +0700

    Update User

--- ClassLib/DTO/User/UpdateUserRequest.cs
+++ ClassLib/DTO/User/UpdateUserRequest.cs
@@ -12,6 +12,8 @@ namespace ClassLib.DTO.User
         public string Name { get; set; } = null!;
         public DateTime DateOfBirth { get; set; }
         public int Gender { get; set; }
+        public string Avatar { get; set; } = null!;
+
         public string Gmail { get; set; } = null!;
         public string PhoneNumber { get; set; } = null!;
     }
