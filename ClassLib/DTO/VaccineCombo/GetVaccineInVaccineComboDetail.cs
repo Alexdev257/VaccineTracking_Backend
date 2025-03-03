3b6991384f5a19a8fdfe84827a9253ec4d4ea4fb
Alexdev257 <bthang2507@gmail.com>
2025-03-03 23:35:27 +0700

    update more function in vaccine Combo

---
+++ ClassLib/DTO/VaccineCombo/GetVaccineInVaccineComboDetail.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib.DTO.VaccineCombo
+{
+    public class GetVaccineInVaccineComboDetail
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public int Quantity { get; set; }
+
+        public string Description { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public int DoesTimes { get; set; }
+
+        public int SuggestAgeMin { get; set; }
+
+        public int SuggestAgeMax { get; set; }
+
+        public DateTime EntryDate { get; set; }
+
+        public DateTime TimeExpired { get; set; }
+
+        public int AddressId { get; set; }
+
+        public string Status { get; set; } = null!;
+
+        public int? MinimumIntervalDate { get; set; }
+
+        public int? MaximumIntervalDate { get; set; }
+
+        public string FromCountry { get; set; } = null!;
+
+        public bool IsDeleted { get; set; }
+    }   
+}
