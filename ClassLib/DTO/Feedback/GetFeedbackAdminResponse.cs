e5534f9c3b47db1bceda28df60dff55661e11871
Alexdev257 <bthang2507@gmail.com>
2025-03-15 18:04:15 +0700

    fix endpoint get-all-feedback-admin

---
+++ ClassLib/DTO/Feedback/GetFeedbackAdminResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib.DTO.Feedback
+{
+    public class GetFeedbackAdminResponse
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int RatingScore { get; set; }
+
+        public string? Description { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
